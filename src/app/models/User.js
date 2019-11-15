041990da1dca02fe924654eefe03398752fdb699
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-15 13:36:26 -0300

    feature(model-user): model responsible to represent a user

---
+++ src/app/models/User.js
@@ -0,0 +1,21 @@
+import Sequelize, { Model } from 'sequelize';
+
+/**
+ * This class represent the user's model
+ */
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
