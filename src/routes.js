ba4e627ac505230615aba468e15259baa8a83807
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-15 15:37:21 -0300

    feature(authentication user): Create session authentication

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
 /**
  * Route to store a user
  */
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
