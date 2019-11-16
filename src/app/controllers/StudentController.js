810f0d398b833cc0aef1f722a9f70beb0edd257c
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-16 19:58:04 -0300

    feature( updateStudent): Update Student

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -6,7 +6,9 @@ class StudentController {
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
-      email: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
       age: Yup.number()
         .integer()
         .positive()
@@ -45,7 +47,58 @@ class StudentController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      oldEmail: Yup.string().email(),
+      email: Yup.string()
+        .email()
+        .when('oldEmail', (oldEmail, field) =>
+          oldEmail ? field.required() : field
+        ),
+      confirmEmail: Yup.string()
+        .email()
+        .when('email', (email, field) => (email ? field.required() : field)),
+
+      age: Yup.number()
+        .integer()
+        .positive(),
+      weight: Yup.number()
+        .integer()
+        .positive(),
+      height: Yup.number()
+        .integer()
+        .positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
+    const { email, oldEmail } = req.body;
+
+    /**
+     * Verify if email is not undefined and if have one student using the email if emai
+     */
+
+    if (email && email !== oldEmail) {
+      const studentExist = await Student.findOne({ where: { email } });
+
+      if (studentExist) {
+        return res.status(400).json({ error: 'Student already exists' });
+      }
+    }
+
+    const student = await Student.findOne({ where: { email: oldEmail } });
+
+    const { name, age, weight, height } = await student.update(req.body);
+
+    return res.json({
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
   }
 }
 
