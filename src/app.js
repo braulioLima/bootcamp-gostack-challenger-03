9d7bcb23dd2f714d456ebe55911b1ae82def66fb
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-22 17:13:17 -0300

    Chore(Sentry): add error report

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import 'dotenv/config';
 
 import express from 'express';
+import 'express-async-errors';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 
 import routes from './routes';
 
@@ -9,12 +13,25 @@ import './database';
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.errorHandler());
     this.server.use(express.json());
   }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn: process.env.SENTRY_DSN,
+  // dsn: 'https://e7e9dfa5cc33450daafb09e644f3eb6f@sentry.io/1831378',
+};
