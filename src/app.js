8d11a9da3e51ac660b1fda0fec81c2e0d17b546a
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-23 13:29:08 -0300

    Chore(error log): fix sentry error log

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ class App {
     this.server.use(async (err, req, res, next) => {
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
+
         return res.status(500).json(errors);
       }
       return res.status(500).json({ error: 'Internal server error' });
@@ -37,6 +38,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 
