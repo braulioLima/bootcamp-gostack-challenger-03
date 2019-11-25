2b83f733a63ef650b0ca08936cb5d4fb3baaa18f
Bráulio F. Lima <braulio.freires.lima@gmail.com>
2019-11-25 02:48:36 -0300

    Feature(list all orders): list all student's order not answerd

--- src/app/controllers/AnswerOrderController.js
+++ src/app/controllers/AnswerOrderController.js
@@ -1,7 +1,10 @@
+import * as Yup from 'yup';
+
 import HelpOrder from '../models/HelpOrder';
+import Student from '../models/Student';
 
 /**
- * Student's controller to help orders
+ * Gym's controller to answer help orders
  */
 class AnswerOrderController {
   async store(req, res) {
@@ -9,7 +12,37 @@ class AnswerOrderController {
   }
 
   async index(req, res) {
-    return res.json();
+    /**
+     * Check if have valid page format
+     */
+    const validPageFormat = Yup.number()
+      .integer()
+      .positive();
+
+    const { page = 1 } = req.query;
+
+    if (!(await validPageFormat.isValid(page))) {
+      return res
+        .status(401)
+        .json({ error: 'Validation Fails, page format incorrect' });
+    }
+
+    const orders = await HelpOrder.findAndCountAll({
+      attributes: ['id', 'question'],
+      limit: process.env.APP_PAGE_SIZE,
+      offset: (page - 1) * process.env.APP_PAGE_SIZE,
+      where: {
+        answer_at: null,
+      },
+      include: {
+        model: Student,
+        as: 'student_data',
+        attributes: ['id', 'name'],
+      },
+      order: [['updated_at', 'DESC']],
+    });
+
+    return res.json({ data: orders, page });
   }
 }
 
